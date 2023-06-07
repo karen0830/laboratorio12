7e0c10ab2a129de4c9eac7cdc72f8f28e516cc1e
karen <lopezkaren43567@gmail.com>
2023-06-07 10:23:33 -0500

    First commit

---
+++ Laboratorio12/src/Aplicacion.java
@@ -0,0 +1,12 @@
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		Entidad rectangulo;
+		rectangulo = new Entidad();
+		
+		rectangulo.ingresarDatos();
+		rectangulo.calcularArea();
+	}
+
+}
